40379a7fa28bdb69d204a6b9b16f3aabc4f5abb6
Grzegorz Sienkiewicz <grzegorz.sienkiewicz95@gmail.com>
2020-03-27 18:46:58 +0100

    Prepare board

--- src/pl/shipproject/Main.java
+++ src/pl/shipproject/Main.java
@@ -3,8 +3,24 @@ package pl.shipproject;
 public class Main {
 
     public static void main(String[] args) {
+        System.out.print("  ");
         for (int i = 0; i < 10 ; i++) {
-            System.out.print('A');
+            System.out.print((char)('A' + i));
+        }
+        System.out.print('\n');
+        for (int i = 1; i <= 10 ; i++) {
+            if(i < 10) {
+                System.out.print(' ');
+            }
+            System.out.println(i);
+            for (int j = 0; j < 10; j++) {
+                if (Math.random() < 0.2){
+                    System.out.print('O');
+                } else {
+                System.out.print(' ');
+                }
+            }
+            System.out.print('\n');
         }
     }
 }
